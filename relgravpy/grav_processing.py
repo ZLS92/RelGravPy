46c16d0936febc0b2778e4c3d6e655e3ad60befc
ZLS92 <zampaluigis@gmail.com>
2026-02-17 15:25:04 +0100

    Convert timestamp to datetime format in TideModel class

--- relgravpy/grav_processing.py
+++ relgravpy/grav_processing.py
@@ -2526,7 +2526,8 @@ class TideModel():
         float, float
             Julian century and hour
         """
-        print( timestamp )
+        if isinstance(timestamp, np.datetime64):
+            timestamp = timestamp.astype('datetime64[ns]').astype(dt_datetime)
         origin_date = datetime(1899, 12, 31, 12, 00, 00)  # Noon Dec 31, 1899
         dt = timestamp - origin_date
         days = dt.days + dt.seconds / 3600. / 24.
